d307a715be0ce346f7729abdcb48abe8efa5e6bb
ZeroKong413 <asdfzxcv5187@gmail.com>
2025-08-15 22:35:04 +0900

    feat: 사용자 추가 기능 수정

--- src/main/java/caps/ssl/checklist/repository/ChecklistRepository.java
+++ src/main/java/caps/ssl/checklist/repository/ChecklistRepository.java
@@ -1,13 +1,13 @@
-package caps.ssl.checklist.repository;
-
-import caps.ssl.checklist.model.Checklist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
-
-    Optional<Checklist> findByContractId(Long contractId);
-}
+//package caps.ssl.checklist.repository;
+//
+//import caps.ssl.checklist.model.Checklist;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
+//
+//    Optional<Checklist> findByContractId(Long contractId);
+//}
